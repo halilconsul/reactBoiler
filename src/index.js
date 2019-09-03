96899a7cfe2ea9ff15af5427873f59e440493fbd
halil <halil_consul@list.ru>
2019-09-03 23:34:05 +0300

    add react-axe for accesability

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById("app"));
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
